427b9aa29795961ec19125a6de1dd07e56e19694
Taj • forcequit <62081155+forcequitOS@users.noreply.github.com>
2025-09-28 22:01:32 -0400

    Updates netcaps

--- netcaps.rb
+++ netcaps.rb
@@ -2,7 +2,7 @@ class Netcaps < Formula
   desc "netcaps - Turn your Caps Lock LED into a network activity light"
   homepage "https://github.com/forcequitOS/netcaps"
   url "https://github.com/forcequitOS/netcaps/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "2c7d1f0242246e3b0316bf8a9709900172f3720c3ccad81f818baf78c6b713c7"
+  sha256 "9b02d543f886e8ff97676a9ba03104cf920733db7f8bcdaaccb13460d5f034e0"
   license "MIT"
 
   depends_on :xcode => :build
